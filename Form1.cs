a0a13d1811dfded183031a02f02f04a94abb5e42
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-12-29 14:43:45 +0000

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int x=0;
+            int y = 0;
+            byte[] b;
+            String gs="";
+            String fname = "";
+            button1.Text= "loading wait...";
+            openFileDialog1.ShowDialog();
+            fname = openFileDialog1.FileName;
+
+            Application.DoEvents();
+            gs = File.ReadAllText(fname);
+            gs = gs.Replace("\0", "\\0");
+            gs = gs.Replace("   ", "\\t");
+            gs = gs.Replace("\n", "\\n");
+            gs = gs.Replace("\r", "\\r");
+            gs = gs.Replace("\"", "\\\"");
+
+            gs = gs + "";
+            textBox1.Text =gs;
+            button1.Text = "open resource";
+        }
+    }
+}
